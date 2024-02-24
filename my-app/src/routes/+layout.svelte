27122d95073ebb931d1a8550f4661a74cf52b9aa
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-24 11:24:42 +0800

    initial commit

--- my-app/src/routes/+layout.svelte
+++ my-app/src/routes/+layout.svelte
@@ -1,5 +1,37 @@
-<script>import "../app.pcss";</script>
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
 
-<div class="h-screen w-full bg-zinc-950">
-    <slot></slot>
-</div>
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">LEETCODE LOGS</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://twitter.com/SkeletonUI"
+					target="_blank"
+					rel="noreferrer"
+				>
+					X
+				</a>
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://github.com/skeletonlabs/skeleton"
+					target="_blank"
+					rel="noreferrer"
+				>
+					GitHub
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,26 +1,9 @@
-<script lang="ts">
-    import * as Accordion from "$lib/components/ui/accordion";
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-    let items = [
-        {
-            name: "linked-list",
-            desc: "hi"
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Welcome to Skeleton.</h2>
+	</div>
+</div>
 
-        }
-    ]
-  </script>
-   
-   <div class="h-full w-full flex flex-col justify-center items-center text-zinc-200">
-    <h1 class="text-5xl">Hello</h1>
-    <Accordion.Root class=" flex flex-col justify-center items-center w-full">
-        {#each items as item}
-        <Accordion.Item value="item-1" class="w-1/5">
-        <Accordion.Trigger>{item.name}</Accordion.Trigger>
-        <Accordion.Content>
-            {item.desc}
-        </Accordion.Content>
-        </Accordion.Item>
-        {/each}
-    </Accordion.Root>
-   </div>
 
