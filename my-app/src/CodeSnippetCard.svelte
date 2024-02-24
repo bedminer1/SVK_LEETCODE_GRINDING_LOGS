3545c01b396af704e440850b5d088e90e69b87e4
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-24 12:14:41 +0800

    Input box for adding solutions and solution display component coded up

---
+++ my-app/src/CodeSnippetCard.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import { CodeBlock } from '@skeletonlabs/skeleton'
+</script>
+
+<div class="card">
+    <header class="card-header">
+        Question
+        <div class="float-right">
+            <button type="button" class="btn btn-sm variant-filled-secondary">Favourite</button>
+            <button type="button" class="btn btn-sm variant-filled-error">Delete</button>
+        </div>
+    </header>
+    <section class="p-4">
+        <CodeBlock language={"Javascript"} code={`let foo = 'bar'`}/>
+    </section>
+</div>
---
+++ my-app/src/routes/topics/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import CodeSnippetCard from "../../CodeSnippetCard.svelte";
+</script>
+
+<div class="flex justify-center w-full">
+    <div class="grid grid-cols-1 gap-4 min-w-full md:min-w-[750px]">
+        <h3 class="text-center py-6">Add Solution</h3>
+        <div class="card p-4 w-full text-token space-y-4">
+            <label class="label">
+                <span>Question</span>
+                <input type="text" class="input" placeholder="Enter Question">
+            </label>
+            <label class="label">
+                <span>Link</span>
+                <input type="text" class="input" placeholder="Enter Link">
+            </label>
+            <label class="label">
+                <span>Code</span>
+                <textarea class="textarea" rows="4" placeholder="Enter Code Here"></textarea>
+            </label>
+        </div>
+        <div class="text-center py-6">
+            <h2>My Solutions</h2>
+        </div>
+        <CodeSnippetCard />
+    </div>
+</div>
