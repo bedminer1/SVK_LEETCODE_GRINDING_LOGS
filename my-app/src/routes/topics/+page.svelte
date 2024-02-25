8bd0e79c9b569c4536d344238bbcb1f03459f95d
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-25 18:56:11 +0800

    more styles

--- my-app/src/routes/topics/+page.svelte
+++ my-app/src/routes/topics/+page.svelte
@@ -1,10 +1,10 @@
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5 text-center flex flex-col items-center">
+<div class="container h-full mx-auto flex justify-center items-center w-full">
+	<div class="space-y-5 text-center flex flex-col items-center w-full">
 		<h2 class="text-6xl mb-10">Topics</h2>
-		<div class="logo-cloud rounded-none grid-cols-3 text-xl">
-			<a href="/topics/dp" class="p-3 hover:text-warning-500 focus:text-surface-500">dynamic programming</a>
-			<a href="/topics/linked-list" class="p-3 hover:text-warning-500 focus:text-surface-500">linked-list</a>
-			<a href="/topics/tree" class="p-3 hover:text-warning-500 focus:text-surface-500">tree</a>
+		<div class="logo-cloud rounded-none grid-row-3 text-xl border-[1px] w-[22%]">
+			<a href="/topics/dp" class="p-3 hover:text-warning-500 focus:text-warning-500 focus:text-lg">dynamic programming</a>
+			<a href="/topics/linked-list" class="p-3 hover:text-warning-500 focus:text-warning-500 focus:text-lg">linked-list</a>
+			<a href="/topics/tree" class="p-3 hover:text-warning-500 focus:text-warning-500 focus:text-lg">binary trees</a>
 		</div>
 	</div>
 </div>
