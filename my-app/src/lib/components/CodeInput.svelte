0752b80b70873d3d885fd4ed1ab86d96e132443d
bedminer1 <alexander.seah.min.rui@gmail.com>
2024-02-25 12:17:35 +0800

    refactors CodeInput into a component

---
+++ my-app/src/lib/components/CodeInput.svelte
@@ -0,0 +1,16 @@
+
+<form class="card p-6 w-full text-token space-y-4 flex gap-3 flex-col items-center" method="post" action="?/create">
+    <label class="label w-full">
+        <span>Question</span>
+        <input type="text" class="input" name="question" id="question" placeholder="Enter Question">
+        </label>
+        <label class="label w-full">
+            <span>Link</span>
+            <input type="text" class="input" name="link" id="link" placeholder="Enter Link">
+        </label>
+        <label class="label w-full">
+            <span>Code</span>
+            <textarea class="textarea" name="code" id="code" rows="4" placeholder="Enter Code Here"></textarea>
+        </label>
+    <button type="submit" class="btn btn-sm variant-filled-tertiary w-1/5">Add Solution</button>
+</form>
--- my-app/src/routes/topics/dp/+page.svelte
+++ my-app/src/routes/topics/dp/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { CodeBlock } from '@skeletonlabs/skeleton'
-    import CodeDisplay from '$lib/components/CodeDisplay.svelte';
+    import CodeDisplayCard from '$lib/components/CodeDisplayCard.svelte';
+    import CodeInput from '$lib/components/CodeInput.svelte';
 
     export let data
 </script>
@@ -9,26 +10,12 @@
 <div class="flex justify-center w-full">
     <div class="grid grid-cols-1 gap-5 p-8 min-w-full md:min-w-[750px]">
         <h3 class="text-center py-6 text-3xl">Just solved a question?</h3>
-        <form class="card p-6 w-full text-token space-y-4 flex gap-3 flex-col items-center" method="post" action="?/create">
-        <label class="label w-full">
-            <span>Question</span>
-            <input type="text" class="input" name="question" id="question" placeholder="Enter Question">
-            </label>
-            <label class="label w-full">
-                <span>Link</span>
-                <input type="text" class="input" name="link" id="link" placeholder="Enter Link">
-            </label>
-            <label class="label w-full">
-                <span>Code</span>
-                <textarea class="textarea" name="code" id="code" rows="4" placeholder="Enter Code Here"></textarea>
-            </label>
-        <button type="submit" class="btn btn-sm variant-filled-tertiary w-1/5">Add Solution</button>
-        </form>
+        <CodeInput />
         <div class="text-center py-6">
             <h2 class="text-2xl pt-9">Your Solutions</h2>
         </div>
         {#each data?.records as snippet}
-        <CodeDisplay {snippet} />   
+        <CodeDisplayCard {snippet} />   
         {/each}
 
     </div>
